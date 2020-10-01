ee3ddaa816e45ffa6c49e563813dd4df891a90a3
michel <michel.desenvolvedorweb@gmail.com>
2020-10-01 01:34:39 -0300

    fitness club

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,40 @@
+//  Slider
+
 let images = document.querySelectorAll('.img-banner');
 let currentImgIndex = 0;
+
+images[currentImgIndex].classList.add('selected');
 
 window.addEventListener('load', start);
 
 function nextImage(){
     images[currentImgIndex].classList.remove('selected');
-
+    
     currentImgIndex++;
-
+    
     if(currentImgIndex >= images.length) currentImgIndex = 0;
-
+    
     images[currentImgIndex].classList.add('selected');
 }
 
 function start(){
-    images[currentImgIndex].classList.add('selected');
-
     setInterval(() => {
         nextImage();
     }, 5000);
 }
+
+// ----------------------------------------------------------------
+
+//  Menu Toggle
+
+const navbarOpen = document.querySelector('.btn-toggle');
+const navbarClose = document.querySelector('.fa-times');
+const navbarLinks = document.querySelector('.navbar-links');
+
+navbarOpen.addEventListener('click', function(){
+    navbarLinks.classList.toggle('open');
+}, false);
+
+navbarClose.addEventListener('click', function(){
+    navbarLinks.classList.toggle('open');
+}, false);
